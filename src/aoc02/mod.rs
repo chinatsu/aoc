ee73a06539e58d3538ca152833bb12162ab9c79e
chinatsu <lolexplode@gmail.com>
2019-12-03 08:13:46 +0100

    Add tests for aoc02

--- src/aoc02/mod.rs
+++ src/aoc02/mod.rs
@@ -1,7 +1,12 @@
-pub fn csv_to_numbers() -> Vec<u8> {
+pub fn csv_to_numbers() -> Vec<u128> {
     csv_from!("aoc02")
-        .map(|val| val.parse::<u8>().unwrap_or(99))
+        .map(|val| val.parse::<u128>().unwrap_or(99))
         .collect()
+}
+
+
+pub fn run(program: &mut Vec<u128>) -> Vec<u128> {
+    program.to_vec()
 }
 
 #[cfg(test)]
