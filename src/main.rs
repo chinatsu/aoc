5fbdc215aea7f1a1f56650253733b6e96b9da99c
chinatsu <lolexplode@gmail.com>
2019-12-03 17:01:54 +0100

    Add resources folder

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,8 @@ mod aoc01;
 mod aoc02;
 
 fn main() {
-    println!("{}", aoc02::part_2())
+    println!("Solution to 01 pt. 1: {}", aoc01::part_1());
+    println!("Solution to 01 pt. 2: {}", aoc01::part_2());
+    println!("Solution to 02 pt. 1: {}", aoc02::part_1());
+    println!("Solution to 02 pt. 2: {}", aoc02::part_2());
 }
